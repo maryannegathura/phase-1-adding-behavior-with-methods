380956785851919c1eaf290ed75a19b3721b88e0
maryannegathura <mariannegathura7@gmail.com>
2024-07-22 20:34:01 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,46 @@
 // Your code here
+// helpers.js
+
+class Cat {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      return `${this.name} says meow!`;
+    }
+  }
+  
+  class Dog {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      return `${this.name} says woof!`;
+    }
+  }
+  
+  class Bird {
+    constructor(name, gender) {
+      this.name = name;
+      this.gender = gender;
+    }
+  
+    speak() {
+      if (this.gender === 'male') {
+        return `It's me! ${this.name}, the parrot!`;
+      } else {
+        return `${this.name} says squawk!`;
+      }
+    }
+  }
+  
+  module.exports = {
+    Cat,
+    Dog,
+    Bird
+  };
+  
